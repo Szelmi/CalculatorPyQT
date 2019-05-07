2a1683b5b9322fb78d0a3d990016cd5c8588cd92
Mateusz <mksyta99@gmail.com>
2019-05-07 18:57:26 +0200

    Dla jaj

---
+++ smieszne/mateusz.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtWidgets
+from ui_calculator import  Ui_Calculator
+
+
+class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
+    firstNum = None
+    userIsTypingSecondNumber = False
+    czyMalo1 = False
+    czyMalo2 = False
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+        self.pushButton_0.clicked.connect(self.digit_pressed)
+        self.pushButton_1.clicked.connect(self.digit_pressed)
+        self.pushButton_2.clicked.connect(self.digit_pressed)
+        self.pushButton_3.clicked.connect(self.digit_pressed)
+        self.pushButton_4.clicked.connect(self.digit_pressed)
+        self.pushButton_5.clicked.connect(self.digit_pressed)
+        self.pushButton_6.clicked.connect(self.digit_pressed)
+        self.pushButton_7.clicked.connect(self.digit_pressed)
+        self.pushButton_8.clicked.connect(self.digit_pressed)
+        self.pushButton_9.clicked.connect(self.digit_pressed)
+
+        self.pushButton_kropka.clicked.connect(self.decimal_pressed)
+        self.pushButton_plusMinus.clicked.connect(self.unary_operation_pressed)
+        self.pushButton_procent.clicked.connect(self.unary_operation_pressed)
+
+        self.pushButton_dodac.clicked.connect(self.binary_operation_pressed)
+        self.pushButton_odjac.clicked.connect(self.binary_operation_pressed)
+        self.pushButton_podzielic.clicked.connect(self.binary_operation_pressed)
+        self.pushButton_pomnozyc.clicked.connect(self.binary_operation_pressed)
+
+        self.pushButton_rownaSie.clicked.connect(self.equals_pressed)
+
+        self.pushButton_usun.clicked.connect(self.clear_pressed)
+
+        self.pushButton_dodac.setCheckable(True)
+        self.pushButton_odjac.setCheckable(True)
+        self.pushButton_pomnozyc.setCheckable(True)
+        self.pushButton_podzielic.setCheckable(True)
+
+    def digit_pressed(self):
+        button = self.sender()
+
+        if (self.pushButton_dodac.isChecked() or self.pushButton_odjac.isChecked() or
+                self.pushButton_pomnozyc.isChecked() or self.pushButton_podzielic.isChecked())\
+                and (not self.userIsTypingSecondNumber):
+            newLabel = format(float(button.text()), '.15g')
+            self.userIsTypingSecondNumber = True
+        else:
+            if('.' in self.label.text()) and (button.text() == '0'):
+                newLabel = format(self.label.text() + button.text(), '.15')
+            else:
+                newLabel = format(float(self.label.text() + button.text()), '.15g')
+
+        self.label.setText(newLabel)
+
+    def decimal_pressed(self):
+        if '.' not in self.label.text():
+            self.label.setText(self.label.text() + '.')
+
+    def unary_operation_pressed(self):
+        button = self.sender()
+        labelNumber = float(self.label.text())
+
+        if button.text() == '+/-':
+            labelNumber = labelNumber * (-1)
+        else:
+            labelNumber = labelNumber * 0.01
+
+        newLabel = format(labelNumber, '.15g')
+        self.label.setText(newLabel)
+
+    def binary_operation_pressed(self):
+        button = self.sender()
+        self.firstNum = float(self.label.text())
+        if (self.firstNum < 10) and (self.firstNum > 0) and ('.' not in self.label.text()):
+            self.czyMalo1 = True
+        button.setChecked(True)
+
+    def equals_pressed(self):
+        secondNum = float(self.label.text())
+        if ('.' not in self.label.text()) and (secondNum < 10) and (secondNum > 0):
+            self.czyMalo2 = True
+
+        if self.pushButton_dodac.isChecked():
+            if self.czyMalo1 is True and self.czyMalo2 is True:
+                self.label.setText("Licz w pamięci!")
+            else:
+                labelNumber = self.firstNum + secondNum
+                newLabel = format(labelNumber, '.15g')
+                self.label.setText(newLabel)
+            self.pushButton_dodac.setChecked(False)
+        elif self.pushButton_odjac.isChecked():
+            labelNumber = self.firstNum - secondNum
+            newLabel = format(labelNumber, '.15g')
+            self.label.setText(newLabel)
+            self.pushButton_odjac.setChecked(False)
+        elif self.pushButton_pomnozyc.isChecked():
+            labelNumber = self.firstNum * secondNum
+            newLabel = format(labelNumber, '.15g')
+            self.label.setText(newLabel)
+            self.pushButton_pomnozyc.setChecked(False)
+        elif self.pushButton_podzielic.isChecked():
+            labelNumber = self.firstNum / secondNum
+            newLabel = format(labelNumber, '.15g')
+            self.label.setText(newLabel)
+            self.pushButton_podzielic.setChecked(False)
+
+        self.userIsTypingSecondNumber = False
+
+    def clear_pressed(self):
+        self.pushButton_dodac.setChecked(False)
+        self.pushButton_odjac.setChecked(False)
+        self.pushButton_pomnozyc.setChecked(False)
+        self.pushButton_podzielic.setChecked(False)
+
+        self.userIsTypingSecondNumber = False
+
+        self.czyMalo2 = False
+        self.czyMalo1 = False
+
+        self.label.setText('0')
+
+
